cd4927a67d2f43e1c9e9bff8f4c6086de5e52895
MK <monish1apr@gmail.com>
2018-11-22 18:59:45 +0530

    add index.js and setup

---
+++ index.js
@@ -0,0 +1,25 @@
+//Required packages
+const Express = require('express');
+const app = Express();
+
+const Mongoose = require('mongoose');
+const Joi = require('joi');
+const config = require('config');
+
+//Required Routes
+
+
+//JWT Check
+
+
+//MongoDB connection
+Mongoose.connect('mongodb://localhost/RTVTSv01')
+    .then( () => console.log('Connected to database...'))
+    .catch(err => console.error('Error in connection', err));
+
+//Express setup
+app.use(Express.json());
+
+//Check if port is set in environment variable else set it to 3000
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port: ${port}...`));
